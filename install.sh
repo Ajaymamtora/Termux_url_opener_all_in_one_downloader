707559db3ac79a63b8660235181282a27a5ace70
Ajaymamtora <ajaymamtora79@gmail.com>
2020-06-26 00:08:01 +0100

    Fixed trim code

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 rm -f "$HOME/bin/termux-url-opener"
 mkdir $HOME/bin
 #Installing dependencies
-pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
+pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y && pkg install rsync -y
 yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install gallery-dl
 
 #Installing the script
