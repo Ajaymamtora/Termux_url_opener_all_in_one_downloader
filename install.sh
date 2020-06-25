9f0e029281fb3a4fb08f50e6587df04b41b9530e
Ajaymamtora <ajaymamtora79@gmail.com>
2020-06-26 00:57:05 +0100

    formatted install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
 #Clean Install
 rm -f "$HOME/bin/termux-url-opener"
 mkdir $HOME/bin
+
 #Installing dependencies
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y && pkg install rsync -y
 yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install gallery-dl
